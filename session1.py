f6791e88f09f4f4721ea930a05853377735117ec
Faisal-Sey <phaisalsey6@gmail.com>
2022-10-21 21:57:54 +0000

    added session 1 file

---
+++ session1.py
@@ -0,0 +1,48 @@
+surname = "issaka"
+_gad = ""
+
+"""
+_gad -- variable
+"hello -- error
+3.5 -- float
+"3.6" -- string
+hy' -- error
+true -- variable
+True -- boolean
+"""
+
+digits = True * 1
+# print(digits)
+# name = input("Enter your name: ")
+# print(name)
+
+# ----input
+# print content
+# accept what the user types
+
+# print("My name is faisal" + surname)
+# print("My brother is something " + surname)
+# print("My name is faisal" + surname)
+# print("My brother is something" + surname)
+# print("My name is faisal " + surname)
+# print("My brother is something " + surname)
+
+
+# boolean -- True or False
+# int -- integer
+# float -- decimals
+# string -- anything in quotes
+
+# ---operators
+# +, -, *, **, %, /, // -- int, float, boolean
+# +, * -- string
+
+# builtin functions
+# int() -- convert to integer
+# float() --- convert to decimal
+# bool() --- convert to boolean
+# str() --- convert to string
+
+
+# Create software to calculate the age of person given the birthyear
+# and display it as "Hurray, you are 19 years old"
